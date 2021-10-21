761cf8a09b595a7a7a316e7952dd9942714abb59
Alekscostam <kowalski121.al@gmail.com>
2021-10-21 22:24:18 +0200

    first commit

---
+++ src/main/java/com/app/elista/EListaApplication.java
@@ -0,0 +1,13 @@
+package com.app.elista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EListaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EListaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/elista/EListaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.elista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EListaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
