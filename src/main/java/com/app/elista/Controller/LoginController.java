a523d60198c60c9eea048b946b26718effc6017c
Alekscostam <kowalski121.al@gmail.com>
2021-10-26 16:30:14 +0200

    test

--- src/main/java/com/app/elista/Controller/LoginController.java
+++ src/main/java/com/app/elista/Controller/LoginController.java
@@ -3,15 +3,14 @@ package com.app.elista.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("loginapp")
+@RequestMapping("login")
 public class LoginController {
     @GetMapping()
     public String login(){
-        return "login";
+        return "loginapp";
     }
 
 
--- src/main/java/com/app/elista/security/config/WebSecurityConfig.java
+++ src/main/java/com/app/elista/security/config/WebSecurityConfig.java
@@ -37,7 +37,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated().and()
                 .formLogin()
-                .loginPage("/loginapp")
+                .loginPage("/login")
                 .permitAll()
                 .defaultSuccessUrl("/message",true)
                 .and()
